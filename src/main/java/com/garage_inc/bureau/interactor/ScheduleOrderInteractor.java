dcbcc75b2b09296bb1e6838227fa347947e3670d
Marcos Vinicius Marcon <marconvcm@gmail.com>
2021-04-07 18:08:42 -0300

    Fix issue with schedule violation

--- src/main/java/com/garage_inc/bureau/interactor/ScheduleOrderInteractor.java
+++ src/main/java/com/garage_inc/bureau/interactor/ScheduleOrderInteractor.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.Random;
 
 @Component
@@ -26,7 +27,11 @@ public class ScheduleOrderInteractor implements Interactor<ScheduleOrderInteract
     }
 
     @Override
-    public Output run(Input input) {
+    public Output run(Input input) throws Exception {
+
+        Optional<Order> optionalOrder = repository.findFirstByScheduledDateTime(input.getScheduledDateTime());
+        if(optionalOrder.isPresent()) { throw new OrderAlreadyBookedOnSameTimeException(); }
+
         Order order = parse(input);
         order = repository.save(order);
         order = sendOtpMessage(order);
@@ -57,6 +62,10 @@ public class ScheduleOrderInteractor implements Interactor<ScheduleOrderInteract
     private String getFreshOtp() {
         return String.format("%06d", rnd.nextInt(999999));
     }
+
+    public static class BusinessException extends Exception {}
+
+    public static class OrderAlreadyBookedOnSameTimeException extends BusinessException {}
 
     public static class Input {
 
